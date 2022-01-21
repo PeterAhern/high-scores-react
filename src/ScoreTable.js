54dc8649f784db83fee55b7c48733c9e8e3ddce3
Peter Ahern <peterahern87@gmail.com>
2022-01-21 17:03:27 -0500

    Updated score tables

--- src/ScoreTable.js
+++ src/ScoreTable.js
@@ -4,23 +4,27 @@ import PlayerScore from "./PlayerScore";
 const ScoreTable = (props) => {
   return (
     <div className="tableContainer">
-      {props.data.sort((a, b)=> a.name > b.name && 1 || -1).map((country, index) => {
-        return (
-          <table className="countryTable">
-            <thead>
-              <tr className="tableHeading" key={index}>High Scores: {country.name}</tr>
-            </thead>
-            <TableHeadings />
-            <tbody>
-              {country.scores
-                .sort((a, b) => (a.s < b.s && 1) || -1)
-                .map((playerScore, index) => (
-                  <PlayerScore key={index} data={playerScore} />
-                ))}
-            </tbody>
-          </table>
-        );
-      })}
+      {props.data
+        .sort((a, b) => a.name > b.name)
+        .map((country, index) => {
+          return (
+            <table className="countryTable">
+              <thead>
+                <tr className="tableHeading" key={index}>
+                  High Scores: {country.name}
+                </tr>
+              </thead>
+              <TableHeadings />
+              <tbody>
+                {country.scores
+                  .sort((a, b) => (a.s < b.s && 1) || -1)
+                  .map((playerScore, index) => (
+                    <PlayerScore key={index} data={playerScore} />
+                  ))}
+              </tbody>
+            </table>
+          );
+        })}
     </div>
   );
 };
